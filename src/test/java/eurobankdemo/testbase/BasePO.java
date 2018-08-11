485d47d152eb638802933e9e5f3b3a3ddb0af760
Michał Żelichowski <mi.zelichowski@gmail.com>
2018-08-11 18:08:01 +0200

    ExtentReports has been added

--- src/test/java/eurobankdemo/testbase/BasePO.java
+++ src/test/java/eurobankdemo/testbase/BasePO.java
@@ -1,9 +1,11 @@
 package eurobankdemo.testbase;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import eurobankdemo.driver.Driver;
-import eurobankdemo.utils.ExtentReportsManager;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
@@ -12,29 +14,42 @@ import org.testng.annotations.BeforeSuite;
 public class BasePO {
     protected static WebDriver driver;
     protected static Logger logger;
-    protected static ExtentReports extent;
+    protected ExtentReports extent;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest report;
 
     public void initDriver() {
         driver = Driver.getInstance();
     }
 
     public void initLogger() {
-        System.setProperty("LOG4J_CONFIGURATION_FILE", "C:\\Users\\mizel\\Desktop\\Java Projects\\eurobankdemo\\log4j2.xml");
+        System.setProperty("LOG4J_CONFIGURATION_FILE", "log4j2.xml");
     }
 
-    public void initExtent() {
-        extent = ExtentReportsManager.getInstance();
+    public void initReports() {
+        System.setProperty("user.dir", "C:\\Users\\mizel\\Desktop\\Java Projects\\eurobankdemo\\src\\test\\resources\\reports\\ExtentReports.html");
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir"));
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Host Name", "Software Testing Material");
+        extent.setSystemInfo("Environment", "Automation Testing");
+        extent.setSystemInfo("User Name", "SomeUser");
+
+        htmlReporter.config().setDocumentTitle("EuroBank Demo Report");
+        htmlReporter.config().setReportName("EuroBank Demo Report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.STANDARD);
     }
 
     @BeforeSuite
     public void startUp() {
         initDriver();
         initLogger();
-        initExtent();
+        initReports();
     }
 
     @AfterSuite
     public void tearDown() {
-//        driver.quit();
+        driver.quit();
     }
 }
--- src/test/java/eurobankdemo/testcases/LoginPOTest.java
+++ src/test/java/eurobankdemo/testcases/LoginPOTest.java
@@ -1,8 +1,10 @@
 package eurobankdemo.testcases;
 
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import eurobankdemo.pageobjects.LoginPO;
 import eurobankdemo.testbase.BasePO;
-import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -15,18 +17,17 @@ public class LoginPOTest extends BasePO {
     private void init() {
         loginPO = PageFactory.initElements(driver, LoginPO.class);
         driver.get("http://demo.eurobank.pl/logowanie_etap_1.html");
-        logger = LogManager.getLogger(LoginPOTest.class);
-        logger.trace("Application started");
     }
 
     @Test
     @Parameters({"id", "password"})
     public void attemptLogin(String id, String password) {
-        logger.trace("Entering login");
         loginPO.enterId(id);
-        logger.trace("Login entered: " + id);
-        logger.trace("Entering password");
+        report = extent.createTest("Attempt to enter login");
+        report.log(Status.PASS, MarkupHelper.createLabel("Login entered successfully", ExtentColor.GREEN));
         loginPO.enterPassword(password);
-        logger.trace("Entered password: " + password);
+        report = extent.createTest("Attempt to enter password");
+        report.log(Status.PASS, MarkupHelper.createLabel("Password entered successfully", ExtentColor.GREEN));
+        extent.flush();
     }
 }
